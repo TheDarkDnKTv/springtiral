033f7c42c75784bc9e727f68957afce81eef8a94
TheDarkDnKTv <x22dnk@hotmail.com>
2021-05-24 20:14:31 +0300

    12. Annotated configuration

--- src/main/java/thedarkdnktv/springtrial/MusicPlayer.java
+++ src/main/java/thedarkdnktv/springtrial/MusicPlayer.java
@@ -1,7 +1,12 @@
 package thedarkdnktv.springtrial;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,10 +14,15 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
+@Scope("singleton") // I know i not needed to specify it
 public class MusicPlayer {
 
 	private IMusic music;
 	
+	@Value("${musicPlayer.name}")
+	private String name;
+	@Value("${musicPlayer.volume}")
+	private int voulme;
 
 	// Inversion of Controll
 	@Autowired
@@ -20,7 +30,33 @@ public class MusicPlayer {
 		this.music = musicIn; 
 	}
 	
+	@PostConstruct
+	public void init() {
+		System.out.println("Init method");
+	}
+	
+	@PreDestroy
+	public void destory() {
+		System.out.println("Destory method");
+	}
+	
 	public String playMusic() {
 		return "Playing: " + music.getSong();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getVoulme() {
+		return voulme;
+	}
+
+	public void setVoulme(int voulme) {
+		this.voulme = voulme;
 	}
 }
