cd20b571c8729f8f665d761f0bf14c0654f5825d
TheDarkDnKTv <x22dnk@hotmail.com>
2021-05-25 01:15:28 +0300

    17. Introduction in controllers

---
+++ src/main/java/thedarkdnktv/springtrial/mvc/controllers/FirstController.java
@@ -0,0 +1,24 @@
+package thedarkdnktv.springtrial.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author TheDarkDnKTv
+ *
+ */
+@Controller
+@RequestMapping("first")
+public class FirstController {
+	
+	@GetMapping("hello")
+	public String helloPage() {
+		return "first/hello";
+	}
+	
+	@GetMapping("goodbye")
+	public String goodByePage() {
+		return "first/goodbye";
+	}
+}
--- src/main/java/thedarkdnktv/springtrial/mvc/controllers/SecondController.java
+++ src/main/java/thedarkdnktv/springtrial/mvc/controllers/SecondController.java
@@ -1,4 +1,4 @@
-package thedarkdnktv.springtrial.mvc;
+package thedarkdnktv.springtrial.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  *
  */
 @Controller
-public class HelloController {
-
-	@GetMapping("hello-world")
-	public String sayHello() {
-		return "hello_world";
+public class SecondController {
+	
+	@GetMapping("exit")
+	public String exit() {
+		return "second/exit";
 	}
 }
