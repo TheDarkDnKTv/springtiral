f9adbab911ee5ede8cf87699c02f936a0db277a6
TheDarkDnKTv <x22dnk@hotmail.com>
2021-05-24 04:28:28 +0300

    6. DI2

--- src/main/java/thedarkdnktv/springtrial/MusicPlayer.java
+++ src/main/java/thedarkdnktv/springtrial/MusicPlayer.java
@@ -8,6 +8,11 @@ public class MusicPlayer {
 	
 	private IMusic music;
 	
+	private String name;
+	private int volume;
+	
+	public MusicPlayer() {}
+	
 	// Inversion of Controll
 	public MusicPlayer(IMusic musicIn) {
 		this.music = musicIn; 
@@ -15,5 +20,25 @@ public class MusicPlayer {
 	
 	public void playMusic() {
 		System.out.println("Playing: " + music.getSong());
+	}
+	
+	public void setMusic(IMusic music) {
+		this.music = music;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
 	}
 }
--- src/main/java/thedarkdnktv/springtrial/TestSpring.java
+++ src/main/java/thedarkdnktv/springtrial/TestSpring.java
@@ -10,6 +10,9 @@ public class TestSpring {
 		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 		musicPlayer.playMusic();
 		
+		System.out.println(musicPlayer.getName());
+		System.out.println(musicPlayer.getVolume());
+		
 		context.close();
 	}
 }
