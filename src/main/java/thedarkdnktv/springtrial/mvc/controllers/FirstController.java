ddf404166458d070bca3ffbbb2b1b01d3c122aea
TheDarkDnKTv <x22dnk@hotmail.com>
2021-05-25 07:13:00 +0300

    19. Request parameters

--- src/main/java/thedarkdnktv/springtrial/mvc/controllers/FirstController.java
+++ src/main/java/thedarkdnktv/springtrial/mvc/controllers/FirstController.java
@@ -1,8 +1,11 @@
 package thedarkdnktv.springtrial.mvc.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author TheDarkDnKTv
@@ -13,12 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 	
 	@GetMapping("hello")
-	public String helloPage() {
+	public String helloPage(HttpServletRequest request) {
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
+		
+		System.out.printf("Hello, %s %s!\n", name, surname);
+		
 		return "first/hello";
 	}
 	
 	@GetMapping("goodbye")
-	public String goodByePage() {
+	public String goodByePage(@RequestParam(value = "name", required = false) String name,  // Response as Bad request in case of no params.
+			@RequestParam String surname) { 												// But set required could not throw anything
+		System.out.printf("Bye, %s %s!\n", name, surname);									// In case of surname, can be used without value/name, it just takes arg name
+		
 		return "first/goodbye";
 	}
 }
