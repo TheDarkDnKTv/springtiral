17306333745fa051c9c10009f3912b18d734f6ec
TheDarkDnKTv <x22dnk@hotmail.com>
2021-05-27 18:34:08 +0300

    27. Moving to JdbcTemplate

--- src/main/java/thedarkdnktv/springtrial/mvc/config/SpringConfig.java
+++ src/main/java/thedarkdnktv/springtrial/mvc/config/SpringConfig.java
@@ -1,10 +1,14 @@
 package thedarkdnktv.springtrial.mvc.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -46,5 +50,20 @@ public class SpringConfig implements WebMvcConfigurer {
 		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
 		resolver.setTemplateEngine(templateEngine());
 		registry.viewResolver(resolver);
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver"); // TODO move to properties
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("testpass");
+		return dataSource;
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
 	}
 }
--- src/main/java/thedarkdnktv/springtrial/mvc/dao/PersonDAO.java
+++ src/main/java/thedarkdnktv/springtrial/mvc/dao/PersonDAO.java
@@ -1,14 +1,10 @@
 package thedarkdnktv.springtrial.mvc.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import thedarkdnktv.springtrial.mvc.models.Person;
@@ -19,106 +15,34 @@ import thedarkdnktv.springtrial.mvc.models.Person;
  */
 @Component
 public class PersonDAO {
+
+	private final JdbcTemplate template;
 	
-	private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
-	private static final String USER = "postgres";
-	private static final String PASS = "testpass";
-	
-	private static Connection connection;
-	
-	static {
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			connection = DriverManager.getConnection(URL, USER, PASS);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	@Autowired
+	public PersonDAO(JdbcTemplate template) {
+		this.template = template;
 	}
 	
 	public List<Person> index() {
-		List<Person> people = new ArrayList<>();
-		
-		try {
-			Statement state = connection.createStatement();
-			String SQL = "SELECT * FROM Person";
-			ResultSet result = state.executeQuery(SQL);
-			
-			while (result.next()) {
-				Person person = new Person();
-				{
-					person.setId(result.getInt("id"));
-					person.setName(result.getString("name"));
-					person.setAge(result.getInt("age"));
-					person.setEmail(result.getString("email"));
-				}
-				people.add(person);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return people;
+		return template.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
 	}
 	
 	public Person show(int id) {
-		Person result = null;
-		
-		try {
-			PreparedStatement state = connection.prepareStatement("SELECT * FROM Person WHERE id=?");
-			state.setInt(1, id);
-			ResultSet vals = state.executeQuery();
-			
-			vals.next(); // TODO ids
-			
-			result = new Person();
-			result.setId(vals.getInt("id"));
-			result.setName(vals.getString("name"));
-			result.setAge(vals.getInt("age"));
-			result.setEmail(vals.getString("email"));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return result;
+		return template.query("SELECT * FROM Person WHERE id=?", new BeanPropertyRowMapper<>(Person.class), new Object[] {id})
+				.stream()
+				.findAny()
+				.orElse(null);
 	}
 	
 	public void save(Person person) {
-		try {
-			PreparedStatement state =  connection.prepareStatement("INSERT INTO Person VALUES(1, ?, ?, ?)");
-			state.setString(1, person.getName());
-			state.setInt(2, person.getAge());
-			state.setString(3, person.getEmail());
-			state.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		template.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
 	}
 	
 	public void update(int id, Person person) {
-		try {
-			PreparedStatement state = connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
-			state.setString(1, person.getName());
-			state.setInt(2, person.getAge());
-			state.setString(3, person.getEmail());
-			state.setInt(4, id);
-			state.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		template.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", person.getName(), person.getAge(), person.getEmail(), id);
 	}
 	
 	public void delete(int id) {
-		try {
-			PreparedStatement state = connection.prepareStatement("DELETE FROM Person WHERE id=?");
-			state.setInt(1, id);
-			state.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		template.update("DELETE FROM Person WHERE id=?", id);
 	}
 }
